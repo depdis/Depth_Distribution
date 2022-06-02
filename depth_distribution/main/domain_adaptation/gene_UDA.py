e3d648e4890b9c7acafaed9197d2da62937fc285
dis <105617984+depdis@users.noreply.github.com>
2022-06-02 10:10:07 +0800

    no message

--- depth_distribution/main/domain_adaptation/gene_UDA.py
+++ depth_distribution/main/domain_adaptation/gene_UDA.py
@@ -154,7 +154,7 @@ def gene_pseudo_labels_3(model, target_loader, output_dir, cfg):
             my_array_u = (c_int * 204800)(*aaa)
             dll = ctypes.cdll.LoadLibrary(str(project_root) + os.sep + 'spatial_prior_algorithm_small.so')
             dll.Add1.restype = ctypes.POINTER(StructPointer_small)
-            array_count = [100, 77, 10, 32, 23, 17, 17]
+            array_count =[1000, 772, 100, 316, 234, 173, 173]
             my_array_count = (c_int * 7)(*array_count)
             p = dll.Add1(my_array_u, my_array_count)
             newlabel = np.array(p.contents.revalue[:])
